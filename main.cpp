9a234d437e9a043fa8b63a92e8974fdbd1069c54
andrew <andrew@mail.com>
2022-01-16 15:02:22 +0200

    Set base sprites

--- main.cpp
+++ main.cpp
@@ -6,19 +6,41 @@ sf::VideoMode vm(viewSize.x, viewSize.y);
 
 sf::RenderWindow window(vm, "Hello SFML", sf::Style::Default);
 
+sf::Texture skyTexture, bgTexture;
+sf::Sprite skySprite, bgSprite;
+
+sf::Texture heroTexture;
+sf::Sprite heroSprite;
+
+void draw()
+{
+    window.draw(skySprite);
+    window.draw(bgSprite);
+    window.draw(heroSprite);
+}
+
+void init()
+{
+    skyTexture.loadFromFile("assets/sky.png");
+    skySprite.setTexture(skyTexture);
+
+    bgTexture.loadFromFile("assets/background.png");
+    bgSprite.setTexture(bgTexture);
+
+    heroTexture.loadFromFile("assets/player_stand.png");
+    heroSprite.setTexture(heroTexture);
+    heroSprite.setPosition(sf::Vector2f(viewSize.x / 2, viewSize.y / 2));
+    heroSprite.setOrigin(heroTexture.getSize().x / 2, heroTexture.getSize().y / 2);
+}
+
 int main()
 {
-    sf::RectangleShape rect(sf::Vector2f(500.f, 300.f));
-    rect.setFillColor(sf::Color::Yellow);
-    rect.setPosition(viewSize.x / 2, viewSize.y / 2);
-    rect.setOrigin(sf::Vector2f(rect.getSize().x / 2, rect.getSize().y / 2));
+    init();
 
     while (window.isOpen())
     {
         window.clear(sf::Color::Red);
-
-        window.draw(rect);
-
+        draw();
         window.display();
 
         sf::Event event;
