f563515c844789b443c3b199eb57247c45e7f2c4
Andrew ND <andrew.nc.dm@gmail.com>
2022-01-16 15:27:38 +0200

    Adjust base input

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ sf::Sprite skySprite, bgSprite;
 sf::Texture heroTexture;
 sf::Sprite heroSprite;
 
+sf::Vector2f playerPosition;
+bool playerMoving = false;
+
 void draw()
 {
     window.draw(skySprite);
@@ -33,22 +36,46 @@ void init()
     heroSprite.setOrigin(heroTexture.getSize().x / 2, heroTexture.getSize().y / 2);
 }
 
+void updateInput()
+{
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+        if (event.key.code == sf::Keyboard::Escape || event.type == sf::Event::Closed) {
+            window.close();
+        }
+
+        if (event.key.code == sf::Keyboard::D) {
+            playerMoving = event.type == sf::Event::KeyPressed;
+        }
+    }
+}
+
+void update(float dt)
+{
+    if (playerMoving) {
+        heroSprite.move(100.f * dt, 0);
+    }
+}
+
 int main()
 {
+    sf::Clock clock;
+
     init();
 
     while (window.isOpen())
     {
+        updateInput();
+
+        sf::Time dt = clock.restart();
+        update(dt.asSeconds());
+
         window.clear(sf::Color::Red);
         draw();
         window.display();
 
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+        
     }
 
     return 0;
