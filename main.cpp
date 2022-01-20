551a9f40c88b3bec937106313262359203187670
Andrew ND <andrew.nc.dm@gmail.com>
2022-01-20 09:28:50 +0200

    Use enemies in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
+
 #include "hero.h"
+#include "enemy.h"
 
 sf::Vector2f viewSize(1024, 768);
 sf::VideoMode vm(viewSize.x, viewSize.y);
@@ -11,12 +14,22 @@ sf::Texture skyTexture, bgTexture;
 sf::Sprite skySprite, bgSprite;
 
 Hero hero;
+std::vector<Enemy*> enemies;
+
+float currentTime;
+float prevTime = 0.f;
+
+void spawnEnemy();
 
 void draw()
 {
     window.draw(skySprite);
     window.draw(bgSprite);
     window.draw(hero.getSprite());
+
+    for (Enemy* enemy : enemies) {
+        window.draw(enemy->getSprite());
+    }
 }
 
 void init()
@@ -28,6 +41,8 @@ void init()
     bgSprite.setTexture(bgTexture);
 
     hero.init("assets/player_stand.png", sf::Vector2f(viewSize.x * .25f, viewSize.y * .5f), 200.f);
+
+    srand((int)time(0));
 }
 
 void updateInput()
@@ -50,6 +65,22 @@ void updateInput()
 void update(float dt)
 {
     hero.update(dt);
+
+    currentTime += dt;
+    if (currentTime >= prevTime + 1.125f) {
+        spawnEnemy();
+        prevTime = currentTime;
+    }
+
+    for (int i = 0; i < enemies.size(); i++) {
+        Enemy *enemy = enemies[i];
+        enemy->update(dt);
+
+        if (enemy->getSprite().getPosition().x < 0) {
+            enemies.erase(enemies.begin() + 1);
+            delete(enemy);
+        }
+    }
 }
 
 int main()
@@ -73,4 +104,36 @@ int main()
     }
 
     return 0;
+}
+
+void spawnEnemy()
+{
+    int randLock = rand() % 3;
+
+    sf::Vector2f enemyPos;
+    float speed;
+
+    switch (randLock)
+    {
+        case 0: {
+            enemyPos = sf::Vector2f(viewSize.x, viewSize.y * .75f);
+            speed = -400;
+        }; break;
+        case 1: {
+            enemyPos = sf::Vector2f(viewSize.x, viewSize.y * .6f);
+            speed = -550;
+        }; break;
+        case 2: {
+            enemyPos = sf::Vector2f(viewSize.x, viewSize.y * .4f);
+            speed = -650;
+        }; break;
+        default: {
+            printf("Incorect y value\n");
+        }; return;
+    }
+
+    Enemy* enemy = new Enemy();
+    enemy->init("assets/zombie_skid.png", enemyPos, speed);
+
+    enemies.push_back(enemy);
 }
